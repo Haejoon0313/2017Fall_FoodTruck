5ed96ac79c229b974054c1b32d7e4b9ccde73931
요한\John <vkvkakal@kaist.ac.kr>
2017-11-14 00:09:27 +0900

    a

--- FoodTruck/app/src/main/java/com/example/john/foodtruck/SearchActivity.java
+++ FoodTruck/app/src/main/java/com/example/john/foodtruck/SearchActivity.java
@@ -1,13 +1,45 @@
 package com.example.john.foodtruck;
 
+import android.app.SearchableInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
+    private Button btnShowLocation;
+    private GpsInfo gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        btnShowLocation = (Button) findViewById(R.id.button4);
+
+        // GPS 정보를 보여주기 위한 이벤트 클래스 등록
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                gps = new GpsInfo(SearchActivity.this);
+                // GPS 사용유무 가져오기
+                if (gps.isGetLocation()) {
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    // GPS 를 사용할수 없으므로
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+
+
     }
 }
