84842c6da71e4a80f26eccf1584657317fa2e99b
정해준 <sam0313@kaist.ac.kr>
2017-11-14 01:54:57 +0900

    서버->앱 푸드트럭 데이터 전송 완료 / 액티비티 형식 구축 완료

--- FoodTruck/app/src/main/java/com/example/john/foodtruck/FT_MenuModifyActivity.java
+++ FoodTruck/app/src/main/java/com/example/john/foodtruck/FT_MenuModifyActivity.java
@@ -1,7 +1,10 @@
 package com.example.john.foodtruck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FT_MenuModifyActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class FT_MenuModifyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ft__menu_modify);
+
+
     }
 }
